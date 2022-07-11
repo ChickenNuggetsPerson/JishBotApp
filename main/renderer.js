c5ccaad4653c3c92a9ff34180b7af6196eedf288
ChickenNuggetPerson <haydenallensteele@gmail.com>
2022-07-11 14:29:42 -0600

    Added Settings Menu

--- main/renderer.js
+++ main/renderer.js
@@ -169,6 +169,26 @@ function searchBtnClick() {
     }
 }
 
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+async function settingsBtnClick() {
+    //ipcRenderer.send('settingsMenu')
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+  
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 document.getElementById('searchFeild').onkeypress = function(e){
     if (!e) e = window.event;
     var keyCode = e.code || e.key;
